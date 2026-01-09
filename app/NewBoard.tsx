fe9a3243075c5c131f1abe1f203b469523921eb5
DomDodge <dominicdodge84@gmail.com>
2026-01-09 15:07:49 -0700

    working projects setup

--- app/NewBoard.tsx
+++ app/NewBoard.tsx
@@ -1,15 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react"
+import { createProject, getSession } from "@/lib/actions";
+
 type NewBoardProps = {
   onClose: () => void;
 };
 
 export default function NewBoard({ onClose }: NewBoardProps) {
+
+  async function handleNewBoard(formData: FormData) {
+    const session = await getSession();
+    const title = formData.get("title")
+
+    if(session != null) {
+      createProject(session.id, title)
+    }
+  }
+
   return (
-    <form>
+    <form action={handleNewBoard}>
       <h2>New Board</h2>
       <label>Title</label>
-      <input type="text" maxLength={30} />
+      <input type="text" name="title" maxLength={30} />
       <div className={"row"}>
         <button type="button" className="cancelBtn" onClick={onClose}>CANCEL</button>
         <button type="submit" className="shareBtn">CREATE</button>
